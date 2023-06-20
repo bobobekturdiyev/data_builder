b1a4b91988e3b0275bcabe277f071fe230e573cd
Bobobek Turdiyev <info@programmer.uz>
2023-06-20 12:55:14 +0500

    init: project

---
+++ example/data_builder_example.dart
@@ -0,0 +1,6 @@
+import 'package:data_builder/data_builder.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/data_builder_test.dart
@@ -0,0 +1,16 @@
+import 'package:data_builder/data_builder.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
